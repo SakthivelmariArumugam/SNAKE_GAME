49a6c4abba43d934e5c2ce496a14f7f482a2c061
SakthivelmariArumugam <sakthivelmaribe@gmail.com>
2024-10-09 14:38:36 +0530

    SECOND Commit

--- Main.java
+++ Main.java
@@ -1,7 +1,15 @@
-public class Main {
-    public static void Main(String args[])
-    {
-        System.out.println("Sakthivelmari");
-    }
+import javax.swing.*;
 
+public class Main {
+    public static void main(String[] args) {
+       int boardWidth=600;
+       int boardHeight=boardWidth;
+
+       JFrame frame=new JFrame("Snake");
+       frame.setVisible(true);
+       frame.setSize(boardWidth,boardHeight);
+       frame.setLocationRelativeTo(null);
+       frame.setResizable(false);
+       frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+    }
 }
